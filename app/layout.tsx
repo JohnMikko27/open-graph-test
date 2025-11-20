97d1aeb2c673d0ccb79ca4d4d0282e9a6469f3bf
Mikko <realmiks27@gmail.com>
2025-11-20 10:45:38 -0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,9 @@ const geistMono = Geist_Mono({
 const baseUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_PROD_URL! : process.env.NEXT_PUBLIC_DEV_URL!;
 
 export const metadata: Metadata = {
-  title: 'SF Hacks 2026 Application Signup',
+  title: 'testttt',
   description:
-    'Sign up to participate in SF Hacks 2026, the premier student hackathon in San Francisco. Submit your application to join the event and showcase your skills!',
+    'testermit your application to join the event and showcase your skills!',
   icons: `${baseUrl}/logo_pink.png`,
   keywords: [
     'SF Hacks 2026',
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     siteName: 'something',
     images: [
       {
-        url: `${baseUrl}/Golden_State_Warriors_logo.svg`,
+        url: `${baseUrl}/public/Golden_State_Warriors_logo.svg`,
         width: 1200,
         height: 630,
         alt: 'SFHacks Hackathon'
@@ -57,7 +57,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex min-h-screen flex-col`}
       >
-        test again i think hopefully it works
+        test again i think hopefully it works  
         {baseUrl}
       </body>
     </html>
