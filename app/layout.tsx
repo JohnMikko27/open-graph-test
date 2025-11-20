d52a470101b592b899129eb24fe80e2a41ada6a8
Mikko <realmiks27@gmail.com>
2025-11-20 10:49:39 -0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     siteName: 'something',
     images: [
       {
-        url: `${baseUrl}/public/Golden_State_Warriors_logo.svg`,
+        url: `${baseUrl}/Golden_State_Warriors_logo.svg`,
         width: 1200,
         height: 630,
         alt: 'SFHacks Hackathon'
